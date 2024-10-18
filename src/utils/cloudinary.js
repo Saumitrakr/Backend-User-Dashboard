50a0cbcd1f3647ff88bf13a452eddf1f310c49bc
Saumitra <2021mmb1358@iitrpr.ac.in>
2024-10-18 16:31:34 +0530

    all setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from fs
+import { response } from "express";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async function(localfilePath){
+
+    try {
+        
+        if(!localfilePath) return null
+
+        const response = await cloudinary.uploader.upload(localfilePath,{
+            resource_type: "auto"
+        })
+
+        console.log("file uploaded successfully", response.url)
+        return response
+
+    } catch (error) {
+
+        fs.unlinkSync(localfilePath)
+        return null
+    }
+
+}
+
+export {uploadOnCloudinary} 
